fe24f447fb5c036174cc928957d52f5ea3aad179
Vadim Fanin <faninv@yahoo.com>
2017-04-14 16:08:25 +0300

    simple widget files added

---
+++ widget-data.php
@@ -0,0 +1,14 @@
+﻿<?php 
+
+//echo Hi;
+
+if(!isset($_GET['callback'])){
+	echo "ERROR: you must pass a callback parameter";
+} else {
+	//header("Content-Type: application/json");
+    echo $_GET['callback'] . '(' . '{"class": "widget-default", "title": "some_title", "description": "some_description", "image": "/some/url.jpg", "avatars": "/some/url.png", "bullets": ""}' . ')';
+
+}
+
+
+?>
