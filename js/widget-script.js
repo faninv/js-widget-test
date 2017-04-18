1a4f92065e8794085056c34e9fc4d4045742cf12
Vadim Fanin <faninv@yahoo.com>
2017-04-18 13:36:18 +0300

    added some dynamic styles

--- js/widget-script.js
+++ js/widget-script.js
@@ -1,75 +1,40 @@
-(function() {
+$(function() {
+    $.widget( "namespace.modernWidget", {
 
-// Localize jQuery variable
-var jQuery;
+        // Default options.
+        options: {
+            width: '200px',
+            height: '200px',
+            backgroundColor: '#fff',
+            fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
+            fontSize: '16px',
+            fontColor: '#000',
+            margin: '0 10px 20px',
+            padding: '10px'
+        },
 
-/******** Load jQuery if not present *********/
-if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.4.2') {
-    var script_tag = document.createElement('script');
-    script_tag.setAttribute("type","text/javascript");
-    script_tag.setAttribute("src",
-        "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-    if (script_tag.readyState) {
-      script_tag.onreadystatechange = function () { // For old versions of IE
-          if (this.readyState == 'complete' || this.readyState == 'loaded') {
-              scriptLoadHandler();
-          }
-      };
-    } else {
-      script_tag.onload = scriptLoadHandler;
-    }
-    // Try to find the head, otherwise default to the documentElement
-    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
-} else {
-    // The jQuery version on the window is the one we want to use
-    jQuery = window.jQuery;
-    main();
-}
+        _create: function() {
 
-/******** Called once jQuery has loaded ******/
-function scriptLoadHandler() {
-    // Restore $ and window.jQuery to their previous values and store the
-    // new jQuery in our local jQuery variable
-    jQuery = window.jQuery.noConflict(true);
-    // Call our main function
-    main(); 
-}
+            var id = this.element.attr('id');
+            var jsonp_url = "http://93.170.169.173/widget-test/widget-data.php?callback=?";
+            $.getJSON(jsonp_url, function(data) {
+                var container = $('#'+ id);
+                container.html("Widget data: <br>title = " + data.title + "<br> description = " + data.description + "<br>");
+            });
 
+            this._refresh();
+        },
 
-function getSyncScriptParams() {
-	var scripts = document.getElementsByTagName('script');
-	var lastScript = scripts[scripts.length-1];
-	var scriptName = lastScript;
-	return {
-		className : scriptName.getAttribute('data-class-name')
-	};
- }
+        _refresh: function() {
 
-/******** Our main function ********/
-function main() { 
-    jQuery(document).ready(function($) { 
-        /******* Load CSS *******/
-        var css_link = $("<link>", { 
-            rel: "stylesheet", 
-            type: "text/css", 
-            href: "http://93.170.169.173/widget-test/css/widget-style.css" 
-        });
-        css_link.appendTo('head');          
-
-        /******* Load HTML *******/
-        var jsonp_url = "http://93.170.169.173/widget-test/widget-data.php?callback=?";
-        $.getJSON(jsonp_url, function(data) {
-			var container = $('#widget-container');
-			var containerClass = 'fw-widget-container';
-
-			if(typeof(getSyncScriptParams()) === "object" && typeof(getSyncScriptParams().className) === "string" && getSyncScriptParams().className !== "")
-				containerClass = containerClass + ' ' + getSyncScriptParams().className;
-			else 
-				containerClass = containerClass + ' fw-widget-default';
-
-			container.addClass(containerClass).html("Widget data: title = " + data.title + ", description = " + data.description);
-        });
+            this.element.css( "width", this.options.width);
+            this.element.css( "height", this.options.height);
+            this.element.css( "background-color", this.options.backgroundColor);
+            this.element.css( "font-family", this.options.fontFamily);
+            this.element.css( "font-size", this.options.fontSize);
+            this.element.css( "color", this.options.fontColor);
+            this.element.css( "margin", this.options.margin);
+            this.element.css( "padding", this.options.padding);
+        }
     });
-}
-
-})();
+});
